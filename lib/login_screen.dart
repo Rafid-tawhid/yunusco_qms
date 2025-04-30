7a9b2d394ba4619b75e6765cb69e564ccb61bd46
Rafid-tawhid <rafidtawhid@gmail.com>
2025-04-30 16:33:16 +0600

    mark to image Done

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -54,9 +54,9 @@ class _LoginScreenState extends State<LoginScreen> {
                   // Logo & Title
                   Image.asset('images/logo.png', height: 140, width: 140),
                   const SizedBox(height: 20),
-                  Text("Welcome Back", style: AppConstants.customTextStyle(18,Colors.white, FontWeight.bold)),
+                  Text("Welcome Back", style: AppConstants.customTextStyle(18, Colors.white, FontWeight.bold)),
                   const SizedBox(height: 8),
-                  Text("Sign in to continue", style: AppConstants.customTextStyle(18,Colors.white.withOpacity(0.8), FontWeight.w600)),
+                  Text("Sign in to continue", style: AppConstants.customTextStyle(18, Colors.white.withOpacity(0.8), FontWeight.w600)),
                   const SizedBox(height: 40),
 
                   // Login Form
@@ -110,7 +110,10 @@ class _LoginScreenState extends State<LoginScreen> {
                         const SizedBox(height: 10),
 
                         // Forgot Password
-                        Align(alignment: Alignment.centerRight, child: TextButton(onPressed: () {}, child: Text("Forgot Password?",style: AppConstants.customTextStyle(12, Colors.white.withOpacity(.7), FontWeight.w400),))),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(onPressed: () {}, child: Text("Forgot Password?", style: AppConstants.customTextStyle(12, Colors.white.withOpacity(.7), FontWeight.w400))),
+                        ),
                         const SizedBox(height: 30),
                         // Login Button
                         SizedBox(
@@ -125,6 +128,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                         setState(() => _isLoading = true);
 
                                         try {
+
                                           var bp = context.read<BuyerProvider>();
                                           bool loginSuccess = await bp.userLogin(_emailCon.text.trim(), _passCon.text.trim());
 
@@ -152,7 +156,7 @@ class _LoginScreenState extends State<LoginScreen> {
                             child:
                                 _isLoading
                                     ? const SizedBox(width: 24, height: 24, child: CircularProgressIndicator(color: Colors.deepPurple, strokeWidth: 2))
-                                    :  Text("LOGIN", style: AppConstants.customTextStyle(16,myColors.primaryColor, FontWeight.w500)),
+                                    : Text("LOGIN", style: AppConstants.customTextStyle(16, myColors.primaryColor, FontWeight.w500)),
                           ),
                         ),
                       ],
